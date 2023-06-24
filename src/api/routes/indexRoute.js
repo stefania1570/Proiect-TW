a43a7480729b7445c8188c38223c6a30ff8cb381
DulhacNinja <cookieziislove@gmail.com>
2023-06-24 09:38:38 +0300

    asdsad

---
+++ src/api/routes/indexRoute.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+function indexRoute(req, res){
+    res.writeHead(200, { "Content-Type": "text/html"});
+    fs.readFile('../views/index.html', 'utf8', function(err, data){
+        res.end(data);
+    });
+      
+    console.log('readFile called');
+    }
+module.exports ={indexRoute}
